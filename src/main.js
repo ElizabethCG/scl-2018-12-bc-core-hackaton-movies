ce2740e9f6585bca3c54b1e1618d75107d37b55d
ElizabethCG <elizabeth.caceres@gmail.com>
2019-01-23 22:49:00 -0300

    Se corrige funcion showData

--- src/main.js
+++ src/main.js
@@ -36,32 +36,34 @@
 
    llamando.then(result => {return result.json();})
    .then(result => {
-    if (result.Type==="movie"){
+    if (result.Type==="movie"&&result.Poster!=="N/A"){
       movies.push(result);
         }
     // ASÍ SE PUEDE ACCEDER A LOS ELEMENTOS DEL ARRAY
+console.log(movies);
 
-    
-   for (let element in movies) {
+ showData(movies);
+
+   // for (let element in movies) {
       // console.log(movies[element]);
-      document.getElementById("root").innerHTML += "<p>" + movies[element].Title + "</p>";
-      document.getElementById("root").innerHTML += "<p>" + movies[element].Genre + "</p>";
-      document.getElementById("root").innerHTML += "<p>" + movies[element].Plot + "</p>";
-      console.log(movies[element].Poster);
-    
-        let variable3=movies[element].Poster;
-      document.getElementById("root").innerHTML += `
-      <div class="target col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
-        <div class="card">
-         <img src="${element.Poster}" class="img">
-        </div>
-      </div>`
+//       document.getElementById("root").innerHTML += "<p>" + movies[element].Title + "</p>";
+//       document.getElementById("root").innerHTML += "<p>" + movies[element].Genre + "</p>";
+//       document.getElementById("root").innerHTML += "<p>" + movies[element].Plot + "</p>";
+//       // console.log(movies[element].Poster);
+// console.log(movies[element]);
+//         let variable3=movies[element].Poster;
+//       document.getElementById("root").innerHTML += `
+//       <div class="target col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
+//         <div class="card">
+//          <img class=tarjeta src="${variable3}" class="img">
+//         </div>
+//       </div>`
+//
+//
+//       document.getElementById("root").innerHTML += "<p>" + movies[element].Year + "</p>";
 
-
-      document.getElementById("root").innerHTML += "<p>" + movies[element].Year + "</p>";
-
-
-    };
+    //
+    // };
    }).catch(err => {
          console.log(err);
    });
@@ -73,21 +75,21 @@
 
  //Mostrando la Data
 const showData = (movies) => {
-  let result = '';
- movies.forEach(element => {
-     containerRoot.innerHTML += `
+  console.log(movies)
+   let result = '';
+ movies.forEach((element,position)=> {
+         containerRoot.innerHTML += `
         <div class="card">
           <div class="box">
             <div class="img">
-        <img src="${element.Poster}" class="img" alt="Movies"></div>
-        <h5 class="card-title">${element.Title}</h5>
-        <p>${element.Genre}</p>
-        <p>${element.Plot}</p>
-        <p>${element.Year}</p>
+
+        <img src="${movies[position].Poster}" class="img" alt="Movies"></div>
+        <h5 class="card-title">${movies[position].Title}</h5>
+        <p>${movies[position].Genre}</p>
+        <p>${movies[position].Plot}</p>
+        <p>${movies[position].Year}</p>
         </div>
       </div>`
   });
-  return result;
+   return result;
 }
-
-
