3ade6c5e93a112b277114f10550ea4fad7abeb77
ElizabethCG <elizabeth.caceres@gmail.com>
2019-01-24 07:38:36 -0300

    Para actualizar deploy

---
+++ src/data.js
@@ -0,0 +1,43 @@
+
+// buscar por (valueYear,valueGenre, year,genre)
+// let category1=genre;
+// let category2=year;
+// let valueCategory1=valueYear;
+// let valueCategory2=valueGenre;
+// let counter=0;
+// if(valueCaterory1<>""){counter+=1};
+// if(valueCaterory2<>""){counter+=1};
+//
+// iterar en el array con forEach + counter para las condiciones por cada elemento que ingresa
+// que recorra por cada elemento del array y vea si cada elemento es igual al valueCategory 1,
+//  si es igual, que almacene la posicion del elemento (pelicula con todos sus datos) en el array
+//
+// for each para iterar por cada pelicula...
+// para realizar la comparacion de los atributos(2)
+// si atributo1 asigno array 1
+// valtor atributo 1
+// si para atributo valor =a par de la movie entonces suma1
+// repite ciclo, si par de atributo2 es igual a pelicula, suma1..
+// repetir tantas veces como larga del array -1
+//
+// si la suma total igual a largo array, entonces .... meter pelicula en nuevo array
+// devolver array e imprimir en pantalla
+
+
+// if(valueCategory2=keyvalue and value category1=keyvalue){push al array resultado}
+// si una key value está vacia...
+// revisar primero qué contenedores vienen con datos...
+//
+//
+//
+//
+// const searchFor = (data, pokeParameter) => {
+//   const finding = data.filter(element => {return element.num.indexOf(pokeParameter) >= 0;});
+//   console.log(typeof finding);
+//   if (finding[0] === undefined){
+//     const finding = data.filter(element => {return element.name.indexOf(pokeParameter) >= 0;});
+//     console.log(typeof finding);
+//     return finding;
+//   }
+//   return finding;
+// }
