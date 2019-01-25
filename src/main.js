f716c560fd877dc05ee9e35fd75431fcf6ef9962
“DanielaEstrada” <dani.strada@gmail.com>
2019-01-25 09:47:44 -0300

    agregando trailer y mejoras de responsive

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 
-   let movies=[];
-   let calling1="https://www.omdbapi.com/?i=tt";
-   let calling2=3104988;
-   let calling3="&apikey=eb4fa0ba";
-   let calling4="&plot=full";
+let movies=[];
+let calling1="https://www.omdbapi.com/?i=tt";
+let calling2=3104988;
+let calling3="&apikey=eb4fa0ba";
+let calling4="&plot=full";
    // let calling5="&genre=comedy";
 
    let repeat = 500
    for (let i=0;i<=repeat;i++){
 
-   calling = fetch(calling1+calling2+calling3+calling4);
+     calling = fetch(calling1+calling2+calling3+calling4);
 
-   calling.then(result => {return result.json();})
-   .then(result => {
-    if (result.Type==="movie" && result.Poster !=="N/A" && result.Plot !== "N/A"){
-      movies.push(result);
-        }
+     calling.then(result => {return result.json();})
+     .then(result => {
+      if (result.Type==="movie" && result.Poster !=="N/A" && result.Plot !== "N/A"){
+        movies.push(result);
+      }
 
 
 
-        /* Función que carga por pantalla la lista de años de forma dinámica */
+      /* Función que carga por pantalla la lista de años de forma dinámica */
 
  /*       let yearsAvailable = window.callingYears(movies);
         // console.log(yearsAvailable);
@@ -40,10 +40,10 @@
              let selectedYear = document.getElementById("selectYear").value;
              console.log(selectedYear);
 
-*/
+             */
 
 
-      if(i===repeat){showData(movies);
+             if(i===repeat){showData(movies);
         // let yearsAvailable = callingYears(movies);
         // console.log(yearsAvailable);
 
@@ -67,43 +67,44 @@ return selected_movies
       let selected_movies = movies.filter(movie => ((movie[atribute]).indexOf(atributeValue) > -1));
       console.log(selected_movies);
       return selected_movies
-     });*/
+    });*/
 
-}
+  }
 
 //});
 
-   }).catch(err => {
-         console.log(err);
-   });
-   calling2+=1;
+}).catch(err => {
+ console.log(err);
+});
+calling2+=1;
 
-   }
+}
 
 
 
-   const containerRoot = document.getElementById("root");
+const containerRoot = document.getElementById("root");
 
   //Mostrando la Data
   const showData = (movies) => {
-  console.log(movies)
-   let result = '';
- movies.forEach((element,position)=> {
+    console.log(movies)
+    let result = '';
+    movies.forEach((element,position)=> {
  // console.log(position);
  // console.log(element);
-            containerRoot.innerHTML += `
-           
-        <div class="card">
-          <div class="box">
-          <div class="img">
-          <a title="Movies" href="https://www.youtube.com/user/trailersyestrenos"><img src="${movies[position].Poster}" class="img" alt="Movies"></div></a>
-        <h5 class="card-title">${movies[position].Title}</h5>
-        <p>Género: ${movies[position].Genre}</p>
-        <p>Sinópsis: ${movies[position].Plot}</p>
-        <p>Año de Estreno: ${movies[position].Year}</p>
+ containerRoot.innerHTML += `
+ <div class="row">
+ <div class="col -xl-4 col-lg-4 col-md-6 col-sm-6 col-12 mx-auto">   
+ <div class="card">
+ <div class="box">
+ <div class="img">
+ <a title="Movies" href="https://www.youtube.com/watch?v=ZQ-YX-5bAs0"><img src="${movies[position].Poster}" class="img" alt="Movies"></div></a>
+ <h5 class="card-title">${movies[position].Title}</h5>
+ <p>Género: ${movies[position].Genre}</p>
+ <p>Sinópsis: ${movies[position].Plot}</p>
+ <p>Año de Estreno: ${movies[position].Year}</p>
 
-        </div>
-      </div>`
-  });
-   return result;
-}
+ </div>
+ </div>`
+});
+    return result;
+  }
